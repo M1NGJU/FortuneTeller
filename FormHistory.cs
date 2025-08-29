c520d575ba2be739d2bd393510f54ebc5024fc1a
minju <s2303@e-mirim.hs.kr>
2025-08-29 11:44:12 +0900

    #10 FormHistrory기능 추가 및 히스토리 로드 개선

--- FormHistory.cs
+++ FormHistory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,48 @@ namespace FortuneTeller
 {
     public partial class FormHistory: Form
     {
-        public FormHistory()
+        List<string> history;
+        Form1 form1;
+
+        public FormHistory(Form1 form)
         {
+            this.form1 = form;
             InitializeComponent();
+            UPdateHistory();
+            
+        }
+
+        private void LoadHistory()
+        {
+            try
+            {
+                string filename = "history.csv";
+                history = File.ReadAllLines(filename).ToList();
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show($"파일이 없어요");
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"권한이 없어요");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"알 수 없는 오류가 발생했어요.");
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            UPdateHistory();
+        }
+
+        private void UPdateHistory()
+        {
+            LoadHistory();
+            lbHistory.Items.Clear();
+            lbHistory.Items.AddRange(history.ToArray());
         }
     }
 }
